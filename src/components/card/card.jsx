df9ad214b1a0d6515116ed2619928fcaacf9496a
danilo Lima Dias <danilo743377@gmail.com>
2020-12-28 16:39:34 -0300

    imagens e nomes aparecendo na tela

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,19 +1,43 @@
-import React, {useEffect, useState} from 'react'
-import {api} from '../../api/api'
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { api } from "../../api/api";
+
+const Section = styled.section`
+  // max-width: 1200px;
+  // display: flex;
+  // align-items: center;
+  // justify-content: center;
+  // flex-wrap: wrap;
+  // margin: 0 auto;
+  // img {
+    
+  //   // display:flex;
+  //   width:500px;
+  //   height: 500px;
+  //   flex: 1;
+  }
+`;
 
 export default function Card() {
-  
-   const Herais =  useEffect(()=>{
-    api.get('')
-    .then((response)=>{
-     const herois= response.data
-     console.log(herois)
-    })
-  },[]);
-  
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    api.get("").then((response) => {
+      setCharacters(response.data.data.results);
+    });
+  }, []);
+
   return (
-    <div>
-      
-    </div>
-  )
+    <Section>
+      {characters.map((character) => (
+        <div>
+          <img
+            src={[character.thumbnail.path + "." + character.thumbnail.extension]}
+            alt=""
+          />
+          <h2>{[character.name]}</h2>
+        </div>
+      ))}
+    </Section>
+  );
 }
